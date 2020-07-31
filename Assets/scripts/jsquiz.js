c99100dc1f0a30136230edf7baef50940d6ae13f
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-07-31 14:24:24 -0700

    add more selectors to quizpages.css and add buildoptions logic to jsquiz.js

--- Assets/scripts/jsquiz.js
+++ Assets/scripts/jsquiz.js
@@ -4,16 +4,17 @@ let jsquiz = [
         num: 1,
         q: "what's your name",
         options: [
-            {
-                id: 1,
-                optiontext: "Abhijeet"
-            }
+            { id: 1, optiontext: "Abhijeet" },
+            { id: 2, optiontext: "Aaroh" },
+            { id: 3, optiontext: "Roheeni" }
         ],
-        correctOption: 1
+        correctOption: 1,
+        difficulty: "easy",
+        category: "Meta Information"
     },
     {
         num: 2,
-        q: "Where does Abhijeet live",
+        q: "GIVEN I am taking a code quiz  WHEN I click the start button  THEN a timer starts and I am presented with a question  WHEN I answer a question",
         options: [
             {
                 id: 1,
@@ -25,20 +26,71 @@ let jsquiz = [
 
 ]
 
-$(document).ready(function () {    
-    $("#homepageimage").delay(500).animate({ left: "0%" }, 2000);
-    $("#homepageimage").fadeOut();
-    $(".screen").delay(800).animate({ width:"0" }, 3000);
-    
-    $('#readMeModalLong').modal();
+// $(document).ready(function () {    
+//     $("#homepageimage").delay(500).animate({ left: "0%" }, 2000);
+//     $("#homepageimage").fadeOut();
+//     $(".screen").delay(800).animate({ width:"0" }, 3000);
+
+//     $('#readMeModalLong').modal();
+// });
+
+//HTML ELEMENTS
+let $quizContainer = document.querySelector(".quiz-container");
+
+
+//TIMER
+let $timer = document.querySelector(".timer");
+let timercontrol, timeleft, timerInterval;
+
+//Score
+let correctCount = 0;
+let incorrectCount = 0;
+
+//qPointer points to question in Quiz Object.
+let qPointer = 0;
+
+document.getElementById("quiz-start").addEventListener("click", function () {
+    document.getElementById("quiz-start").style.display = "none";
+    $quizContainer.style.display = "block";
+    // timeleft = (jsquiz.length * 20);
+    // timerInterval = setInterval(() => {
+    //     $timer.innerText=timeleft;
+    //     timeleft--;
+    //     timeleft <0 ? clearInterval(timerInterval):0;
+    // }, 1000);
+
+    $timer.style.display = "block";
+    displayQuestion(qPointer);
 });
 
-let $qnum=document.getElementById("qnum");
-$qnum.innerText=100;
+function displayQuestion(qId) {
+    //HTML ELEMENTS
+    let $qnum = document.getElementById("qnum");
+    let $qtext = document.getElementById("qtext");
+    let $qdifflevel = document.getElementById("dlevel");
+    let $qcat = document.getElementById("category");
 
-// let $quizContainer = document.querySelector(".quiz-container");
+    $qnum.innerText = jsquiz[qPointer].num;
+    $qtext.innerText = jsquiz[qPointer].q;
+    $qdifflevel.innerText = jsquiz[qPointer].difficulty.toUpperCase();
+    $qcat.innerText = jsquiz[qPointer].category;
+    buildOptions(qPointer);
+}
 
-// let questionDiv = document.createElement("div");
-// //alert(jsquiz[0].num + jsquiz[0].q);
-// questionDiv.textContent=jsquiz[0].num + jsquiz[0].q;
-// $quizContainer.appendChild(questionDiv);
+function buildOptions(qId) {
+    let $ansOptions = document.getElementById("options");
+    // let $li = document.createElement("li");
+    while($ansOptions.firstElementChild){
+        $ansOptions.removeChild($ansOptions.lastChild);
+    }
+    jsquiz[qId].options.forEach(element => {
+        let $optBtn =document.createElement("button");        
+        $optBtn.classList.add("btn","btn-primary","btn-md","btn-block");
+        $optBtn.innerText = element.optiontext;
+        $optBtn.addEventListener()
+        // $li.appendChild($optBtn);
+        $ansOptions.appendChild($optBtn);
+    });
+
+}
+
