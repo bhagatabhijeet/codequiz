1eaa92330dfae6a18292c301b5215de46c4f1915
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-07-31 17:48:40 -0700

    first working prototype.Needs a lot of refactoring and cleansing.

--- Assets/scripts/jsquiz.js
+++ Assets/scripts/jsquiz.js
@@ -21,7 +21,9 @@ let jsquiz = [
                 optiontext: "Abhijeet"
             }
         ],
-        correctOption: 1
+        correctOption: 1,
+        difficulty: "hard",
+        category: "Meta"
     }
 
 ]
@@ -46,24 +48,30 @@ let timercontrol, timeleft, timerInterval;
 let correctCount = 0;
 let incorrectCount = 0;
 
-//qPointer points to question in Quiz Object.
-let qPointer = 0;
+
 
 document.getElementById("quiz-start").addEventListener("click", function () {
     document.getElementById("quiz-start").style.display = "none";
     $quizContainer.style.display = "block";
-    // timeleft = (jsquiz.length * 20);
-    // timerInterval = setInterval(() => {
-    //     $timer.innerText=timeleft;
-    //     timeleft--;
-    //     timeleft <0 ? clearInterval(timerInterval):0;
-    // }, 1000);
-
+    //qPointer points to question in Quiz Object.
+    let qPointer = 0;
+    timeleft = (jsquiz.length * 20);
+    
     $timer.style.display = "block";
+    $timer.innerText=timeleft;
+
+    //Start timer count down.
+    timerInterval = setInterval(() => {
+        $timer.innerText=timeleft;
+        timeleft--;
+        timeleft <0 ? clearInterval(timerInterval):0;
+    }, 1000);
+
+    //Display First Question - see qPointer==0;
     displayQuestion(qPointer);
 });
 
-function displayQuestion(qId) {
+function displayQuestion(qPointer) {
     //HTML ELEMENTS
     let $qnum = document.getElementById("qnum");
     let $qtext = document.getElementById("qtext");
@@ -74,23 +82,75 @@ function displayQuestion(qId) {
     $qtext.innerText = jsquiz[qPointer].q;
     $qdifflevel.innerText = jsquiz[qPointer].difficulty.toUpperCase();
     $qcat.innerText = jsquiz[qPointer].category;
-    buildOptions(qPointer);
+    
+    //Prepare Answer Option Buttons
+    buildAndDisplayOptions(qPointer);
 }
 
-function buildOptions(qId) {
+function buildAndDisplayOptions(qPointer) {
     let $ansOptions = document.getElementById("options");
-    // let $li = document.createElement("li");
-    while($ansOptions.firstElementChild){
-        $ansOptions.removeChild($ansOptions.lastChild);
-    }
-    jsquiz[qId].options.forEach(element => {
+    let ansOptionsArray=[];
+    
+    cleanOldAnswerOptions($ansOptions);
+
+    // while($ansOptions.firstChild){
+    //     $ansOptions.removeChild($ansOptions.lastChild);
+    // }
+    jsquiz[qPointer].options.forEach(element => {
         let $optBtn =document.createElement("button");        
-        $optBtn.classList.add("btn","btn-primary","btn-md","btn-block");
+        $optBtn.classList.add("btn","btn-md","btn-block");
+        $optBtn.setAttribute("data-id",element.id);
         $optBtn.innerText = element.optiontext;
-        $optBtn.addEventListener()
+        $optBtn.addEventListener("click",function(){
+            let ansId=this.getAttribute("data-id");                        
+            
+            let $feedback=document.getElementById("feedback");
+            
+
+            ansOptionsArray.forEach(e=>{e.disabled=true});
+
+            $feedback.style.display="block";
+            let t=0;
+            
+            let feedbackdisplayInterval = setInterval(function(){
+                t++;
+                if(t >= 1)
+                {
+                    clearInterval(feedbackdisplayInterval);
+                    $feedback.style.display="none";
+                    timeleft+=1;
+                    qPointer++;
+            
+                    displayQuestion(qPointer) ;
+                    $("#feedback").fadeOut(1000);
+                }
+               
+            },1000);
+            
+     
+            if(ansId == jsquiz[qPointer].correctOption){
+                // $feedback.style.color="#19613e";
+                $feedback.style.color="white";
+                $feedback.innerHTML="<i class='fa fa-check-circle' aria-hidden='true'>Correct</i>";
+            }
+            else{
+                // $feedback.style.color="#e0422a";
+                $feedback.style.color="white";
+                $feedback.innerHTML="<i class='fa fa-times-circle-o' aria-hidden='true'>Incorrect</i>";
+            }
+                   
+        });
         // $li.appendChild($optBtn);
+        ansOptionsArray.push($optBtn);
         $ansOptions.appendChild($optBtn);
     });
+}
 
+function cleanOldAnswerOptions(ansOptionDiv){
+    let $ansOptions = document.getElementById("options");
+        
+    while(ansOptionDiv.firstChild){
+        ansOptionDiv.removeChild(ansOptionDiv.lastChild);
+    }
 }
 
