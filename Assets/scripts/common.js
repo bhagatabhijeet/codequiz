cd547bc3806a00fb5cd5f1d727ab8c6d12fdfd6f
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-08-03 14:44:21 -0700

    Add documentation comment to all objects, functions, methods and important statements and calls

--- Assets/scripts/common.js
+++ Assets/scripts/common.js
@@ -1,27 +1,60 @@
 //Quiz Object
+/**
+ * This it the main object that holds quiz stats, question source, current question pointer and time left.
+ */
 let quiz = {
+    /**
+     * questionSource will be set by quiz JavaScript files. questionSource will be Array of question Objects.
+     * ****************   questionSource example    ****************
+     * let pracquiz = [
+     * {
+     *   num: 1,
+     *   q: "1 + 11 + 111 = ___",
+     *   options: [
+     *       { id: 1, optiontext: "121" },
+     *       { id: 2, optiontext: "111" },
+     *       { id: 3, optiontext: "123" }
+     *   ],
+     *   correctOption: 3,
+     *   difficulty: "easy",
+     *   category: "Math"
+     * }
+     * ]
+     * *************************************************************
+     */
     questionSource: null,
+    //quzName property is used for storing the score in localStorage
     quizName:"",
+    
+    //quizStat property is used to maintain the quiz Statistics. Its an object of stats counter.
     quizStat: {
         incorrectCount: 0,
         correctCount: 0,
         score: 0
     },
+
+    //timeleft property for quizTimer
     timeleft: 0,
+
+    //initialTimeleft is calculated dynamically based on difficulty level of question
+    //like 15 seconds for EASY, 25 seconds for MEDIUM and 45 seconds for HARD.
     initialTimeLeft: 0,
 
-    //qPointer points to current Question
+    //qPointer points to ** CURRENT QUESTION **
     qPointer: 0,
+
+    //method called by quiz JavaScipts to set the name of quiz. The name is used to store score in local storage
     setQuizName:function(name){
         this.quizName=name;
     },
+
+    //method called by quiz JavaScipts to set the questionSource of quiz.
     setQuestionSource: function (qObject) {
         this.questionSource = qObject;
         document.getElementById("quiz-start").addEventListener("click", startQuiz);
     },
-    // setTimeLeft:function(n){
-    //     this.timeleft=n;
-    // },
+
+    //method to calculate initial time left for the quiz
     calculateInitialTimeLeft: function () {
         this.questionSource.forEach(element => {
             //15 Seconds for EASY
@@ -49,11 +82,19 @@ let $scoreContainer = document.querySelector(".score-container");
 let $clearHighScore = document.getElementById("clearHighScores")
 let $scoreTable = document.getElementById("scoretable");
 
-//Add every button  created to ansOptionsArray.
-// This will be used to ** DISABLE ** buttons once the user clicks an answer.
-// We do not want user to cheat so once answered disable all buttons.
+/** 
+ * Add every button  created to ansOptionsArray.
+ * This will be used to ** DISABLE ** buttons once the user clicks an answer.
+ * We do not want user to cheat  :) so once answered disable all buttons.
+ */
 let ansOptionsArray = [];
 let quizTimerInterval;
+
+
+/**
+ * startQuiz is the main function which triggers the quiz.
+ * startQuiz is called upon clicking the 'Start Quiz' button.
+ */
 
 function startQuiz() {
     //get the initial time left from the question source. 
@@ -62,9 +103,12 @@ function startQuiz() {
 
     prepareUI();
     quizTimer();
-    displayQuestion();
-    // alert(quiz.questionSource.length);
+    displayQuestion();    
 }
+
+/**
+ * perpareUI hides the 'Start Quiz' button and displays the quiz-container.
+ */
 
 function prepareUI() {
     document.getElementById("quiz-start").style.display = "none";
@@ -73,17 +117,23 @@ function prepareUI() {
     $timer.innerText = quiz.timeleft;
 }
 
+/**
+ * quizTimer is a function to set the quizTimer interval and show visual indicators for timer
+ * like the timer color is green initially but as the time left becomes less the color changes to 
+ * orange and finally a red border around the timer is set if the time left is less than 10% or 10sec.
+ */
+
 function quizTimer() {
     quizTimerInterval = setInterval(() => {
-        //Time left is more than 50% of total available time - GREEN
+        // Time left is more than 50% of total available time - GREEN
         if (quiz.timeleft > 0.5 * quiz.initialTimeLeft) {
             $timer.style.color = "green";
         }
-        //Time left is more than 10%  bt less than 50% of total available time - ORANGE
+        // Time left is more than 10%  bt less than 50% of total available time - ORANGE
         if (quiz.timeleft > 0.1 * quiz.initialTimeLeft && quiz.timeleft < 0.5 * quiz.initialTimeLeft) {
             $timer.style.color = "orange";
         }
-        //Time left is less than 10% of total available time or last 10 seconds - Red
+        // Time left is less than 10% of total available time or last 10 seconds - Red
         if (quiz.timeleft < 0.1 * quiz.initialTimeLeft || quiz.timeleft <= 10) {
             $timer.style.color = "Red";
             $timer.style.border = "2px solid red";
@@ -98,6 +148,11 @@ function quizTimer() {
     }, 1000);
 }
 
+/**
+ * displayQuestion will prpare UI , display question and will give call to a function to build 
+ * answer option buttons.
+ */
+
 function displayQuestion() {
     //HTML ELEMENTS
     let $qnum = document.getElementById("qnum");
@@ -110,10 +165,14 @@ function displayQuestion() {
     $qdifflevel.innerText = quiz.questionSource[quiz.qPointer].difficulty.toUpperCase();
     $qcat.innerText = quiz.questionSource[quiz.qPointer].category;
 
-    //Prepare Answer Option Buttons
+    // Prepare Answer Option Buttons
     buildAndDisplayOptions();
 }
 
+/**
+ * buildAndDisplayOptions is a function to dynamically add option buttons below the questions.
+ * the option buttons' text is fetched from the questionSource onject
+ */
 
 function buildAndDisplayOptions() {
     ansOptionsArray = [];
@@ -142,7 +201,7 @@ function buildAndDisplayOptions() {
     });
 }
 
-
+// A function to call when a user clicks the answer option.
 function answerClicked() {
     let ansId = this.getAttribute("data-id");
 
@@ -155,12 +214,18 @@ function answerClicked() {
 
 }
 
+// remove buttons which were dynamically added earlier.
 function cleanOldAnswerOptions() {
     while ($ansOptions.firstChild) {
         $ansOptions.removeChild($ansOptions.lastChild);
     }
 }
 
+/**
+ * checkAnswer is used to check the answer, add quizStats and show quick feedback
+ * if the user has answered this function will be used to move to next question.
+ * Also if all the questions have been answered or no time is left this function will call the saveScore form
+ */
 function checkAnswer(ansId) {
 
     let $feedback = document.getElementById("feedback");
@@ -173,17 +238,18 @@ function checkAnswer(ansId) {
     }
     else {
         quiz.quizStat.incorrectCount++;
-        //10 seconds subtracted for incorrect answer;
+        //10 seconds SUBTRACTED for incorrect answer;
         quiz.timeleft = quiz.timeleft - 10;
         $feedback.style.color = "white";
         $feedback.innerHTML = "<i class='fa fa-times-circle-o' aria-hidden='true'>Incorrect</i>";
     }
-
-    // $feedback.style.display="none";
+    
     quiz.timeleft += 1;
+
+    // progress the qPointer by 1
     quiz.qPointer++;
 
-    //Next Question
+
     if (quiz.timeleft <= 0) {
         $("#feedback").fadeOut(2000);
         setTimeout(showScoreAndSaveForm, 2000, "timeout");
@@ -194,7 +260,7 @@ function checkAnswer(ansId) {
         $("#feedback").fadeOut(2000);
         quiz.timeleft += 1;
 
-        //**** NEXT QUESTION *****/
+        // **** NEXT QUESTION *****/
         setTimeout(displayQuestion, 1500);
 
     }
@@ -216,9 +282,13 @@ function showScoreAndSaveForm(msg) {
     if (msg == "completed") {
         $completionmessage.textContent = "All Questions Answered"
     }
-    //10 points for correct answer
-    //-5 points for incorrect answer
-    //-8 points for non attempted question.
+    /**
+     * score calculation rules
+     * 10 points for correct answer
+     * -5 points for incorrect answer
+     * -8 points for non attempted question.
+     */
+    
     let unattempted = quiz.questionSource.length - (quiz.quizStat.correctCount + quiz.quizStat.incorrectCount)
     let score = quiz.quizStat.correctCount * 10 - quiz.quizStat.incorrectCount * 5 - unattempted * 8;
     $scoremessage.textContent = "Your Score is " + score;
@@ -231,14 +301,24 @@ function showScoreAndSaveForm(msg) {
     $submitBtn.addEventListener("click", submitScore);
 }
 
+/**
+ * submitScore function is used to submit userScore to localStorage
+ * The scores are stored as a Stringified score object
+ * for every quiz a local Storage object with name "cq" + quiz.quizName is created
+ * The scores object is an Array of object
+ * The object properties are username,score,time.
+ */
+
 function submitScore(e) {
     let $user = document.getElementById("initial").value;
     let cqScoresObj;
     //prevent defualt
     e.preventDefault();
 
+    // get localStorage item
     let cqScores = localStorage.getItem("cq"+ quiz.quizName);
 
+    // if the localStorgae item does not exist then create a new item and set it.
     if (cqScores === null) {
         cqScoresObj =[{
             username: $user,
@@ -248,6 +328,8 @@ function submitScore(e) {
         cqScores = JSON.stringify(cqScoresObj);
         localStorage.setItem("cq" + quiz.quizName, cqScores);
     }
+    // if the item exists then add score to the object(use array unshift)
+    // ** SORT and SLICE the array to only 10 to show 10 high scores
     else {
         cqScoresObj = JSON.parse(cqScores);
         cqScoresObj.unshift({
@@ -263,16 +345,11 @@ function submitScore(e) {
         localStorage.setItem("cq" + quiz.quizName, cqScores);
     }
 
-
-    // localStorage.setItem($user, quiz.quizStat.score);
-    // alert($user + " submitting score");
-
-    // $highscorecontainer = document.querySelector(".highscorecontainer");
-    // $scoreContainer.display = "none";
-    // $highscorecontainer.display = "block";
     showHighScore(cqScoresObj);
 }
 
+
+// utility function to quicly generate DateTime string.
 function getDateTimeString() {
     let today = new Date();
     let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
@@ -281,6 +358,7 @@ function getDateTimeString() {
     return dateTime;
 }
 
+// showHighscore function displays the scores in tabular format
 function showHighScore(cqScoresObj) {
     $highscorecontainer = document.querySelector(".highscorecontainer");
     $scoreContainer.style.display = "none";
@@ -302,12 +380,7 @@ function showHighScore(cqScoresObj) {
 
 }
 
-// function clearHighScores()
-// {
-//     localStorage.removeItem("cqscores");
-//     $scoreTable.remove();
-// }
-
+// Event handler for 'Clear High Score' button.
 $clearHighScore.addEventListener("click", function () {
     localStorage.removeItem("cq"+ quiz.quizName);
     $scoreTable.remove();
